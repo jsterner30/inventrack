26ca3c7fd1985bd8b0c68368a2c6437c88694630
Caleb Hill <chill389cc@gmail.com>
2024-09-21 14:45:01 -0600

    remove testing code

--- shared/src/model/add.model.ts
+++ shared/src/model/add.model.ts
@@ -1,13 +1,13 @@
 import { Static, Type } from '@fastify/type-provider-typebox'
 
 export const AddResponseSchema = Type.Object({
-  result: Type.String()
+  result: Type.Number()
 })
 export type AddResponse = Static<typeof AddResponseSchema>
 
 export const AddRequestSchema = Type.Object({
   a: Type.Number(),
-  b: Type.String()
+  b: Type.Number()
 })
 export type AddRequest = Static<typeof AddRequestSchema>
 
