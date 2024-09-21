32bc66198e6aa0fc632269e03ce88eda944b52ca
Caleb Hill <chill389cc@gmail.com>
2024-09-21 13:35:10 -0600

    workspaces

--- server/src/routes/add/root.ts
+++ server/src/routes/add/root.ts
@@ -1,8 +1,9 @@
 import { TypeBoxTypeProvider, FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
 import { AddResponseSchema, AddRequestSchema } from 'shared'
+import { FastifyPluginAsync } from 'fastify'
 
 // maybe use a generic argument for FastifyPluginAsync if we use options with fastify instance
-const add: FastifyPluginAsyncTypebox = async (fastifyApp, { }): Promise<void> => {
+const add: FastifyPluginAsync = async (fastifyApp, { }): Promise<void> => {
     const fastify = fastifyApp.withTypeProvider<TypeBoxTypeProvider>()
 
     fastify.get('/', {
@@ -14,6 +15,9 @@ const add: FastifyPluginAsyncTypebox = async (fastifyApp, { }): Promise<void> =>
         }
     }, async (request, reply) => {
         const { a, b } = request.query
+        const foobar: string = 'hi'
+        const newfoundthing: number = request.query.a * 5
+        const ff = request.query
         reply.status(200).send({ result: a + b})
     })
 }
--- shared/src/model/add.model.ts
+++ shared/src/model/add.model.ts
@@ -1,13 +1,13 @@
-import { Static, Type } from '@sinclair/typebox'
+import { Static, Type } from '@fastify/type-provider-typebox'
 
 export const AddResponseSchema = Type.Object({
-  result: Type.Number()
+  result: Type.String()
 })
 export type AddResponse = Static<typeof AddResponseSchema>
 
 export const AddRequestSchema = Type.Object({
   a: Type.Number(),
-  b: Type.Number()
+  b: Type.String()
 })
 export type AddRequest = Static<typeof AddRequestSchema>
 
