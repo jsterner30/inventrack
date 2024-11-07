0627453fab376b89737c2c2c8fa34623e97bab26
Savannah Smith <s.ella@me.com>
2024-11-06 22:16:23 -0700

    I hate lint

--- web/src/components/AccountSwitcher.tsx
+++ web/src/components/AccountSwitcher.tsx
@@ -7,14 +7,14 @@ interface Props {
 
 export const AccountSwitcher: React.FC<Props> = ({ companyName, avatarUrl }) => {
   return (
-    <div style={{ background: '#BEB8A7', borderRadius: '8px'}}>
+    <div style={{ background: '#BEB8A7', borderRadius: '8px' }}>
       <img
         src={avatarUrl}
         alt='Company Avatar'
         className='rounded-circle me-2'
         style={{ width: '45px', height: '45px' }}
       />
-      <span style={{padding: '5px'}}>{companyName}</span>
+      <span style={{ padding: '5px' }}>{companyName}</span>
     </div>
   )
 }
