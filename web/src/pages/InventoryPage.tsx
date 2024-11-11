6ae0ad76b980dcf239298d645460f51ed2ffb27e
Caleb Hill <chill389cc@gmail.com>
2024-11-11 13:48:43 -0700

    typo: remove ugly backticks

--- web/src/pages/InventoryPage.tsx
+++ web/src/pages/InventoryPage.tsx
@@ -18,7 +18,7 @@ export const InventoryPage: React.FC = () => {
       <Header logoUrl='https://cdn.builder.io/api/v1/image/assets/TEMP/b6715c39bd9a8756258d5ef40fce925081f072fe4db40e56a069bb5a65b1174b?placeholderIfAbsent=true&apiKey=c71af66328b44f89bad6bec599ea2336' />
 
       <main>
-        <h1 style={{ paddingTop: '50px', paddingBottom: '20px', textAlign: 'center' }}>``
+        <h1 style={{ paddingTop: '50px', paddingBottom: '20px', textAlign: 'center' }}>
           Inventory: All Locations
         </h1>
 
