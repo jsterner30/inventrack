7bb9bff58b8c00f7ccb356216c72f6d27ed31820
Caleb Hill <chill389cc@gmail.com>
2024-11-11 13:49:35 -0700

    fix: backend throws 500 instead of empty array when no products

--- shared/src/model/response.model.ts
+++ shared/src/model/response.model.ts
@@ -1,9 +1,10 @@
 import { Static, Type } from '@fastify/type-provider-typebox'
+import { Nullable } from './misc'
 
 export const PageInfoSchema = Type.Object({
   hasNextPage: Type.Boolean(),
   hasPreviousPage: Type.Boolean(),
-  startCursor: Type.String(),
-  endCursor: Type.String()
+  startCursor: Nullable(Type.String()),
+  endCursor: Nullable(Type.String())
 })
 export type PageInfo = Static<typeof PageInfoSchema>
