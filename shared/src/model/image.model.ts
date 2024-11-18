18139929796b1eef018ec22e2faceecba5f5e48f
Caleb Hill <chill389cc@gmail.com>
2024-11-18 13:31:31 -0700

    make image id optional
    There is one of the snowboards (the 16th in the list) that doesn't have
    ids for its images which causes the backend query to fail.

--- shared/src/model/image.model.ts
+++ shared/src/model/image.model.ts
@@ -2,6 +2,6 @@ import { Static, Type } from '@fastify/type-provider-typebox'
 
 export const ImageObjSchema = Type.Object({
   url: Type.String(),
-  id: Type.String()
+  id: Type.Optional(Type.String())
 })
 export type ImageObj = Static<typeof ImageObjSchema>
